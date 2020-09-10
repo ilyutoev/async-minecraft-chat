df90db25df206e98ce5f29b77a8decd63d1d9dcb
ilyutoev <ilyutoev@gmail.com>
2020-09-10 16:24:59 +0300

    Сохраняем историю сообщений в файл

--- listen-minechat.py
+++ listen-minechat.py
@@ -1,16 +1,20 @@
 import asyncio
+from datetime import datetime
+
+import aiofiles
 
 
 async def main():
     reader, writer = await asyncio.open_connection('minechat.dvmn.org', 5000)
 
-    while True:
-        data = await reader.readline()
-        if not data:
-            break
-        print(data.decode().strip())
+    async with aiofiles.open('chat_history.txt', mode='a') as f:
+        while True:
+            data = await reader.readline()
+            if not data:
+                break
 
-    writer.close()
+            await f.write(f'[{datetime.now().strftime("%d.%m.%Y %H:%M")}] {data.decode()}')
+        writer.close()
     await writer.wait_closed()
 
 asyncio.run(main())
