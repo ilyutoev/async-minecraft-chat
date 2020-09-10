5e298412cd2cc9d62759aabe4422fc7786b05bbb
ilyutoev <ilyutoev@gmail.com>
2020-09-10 22:41:17 +0300

    Рефакторинг. Разделение функционала на отдельые функции

--- write-minechat.py
+++ write-minechat.py
@@ -11,52 +11,73 @@ CHAT_HASH = 'c11a9f9e-f36f-11ea-8c47-0242ac110002'
 logging.basicConfig(level=logging.DEBUG)
 
 
-async def main():
+async def authorise(writer, reader, token):
+    """Функция для авторизации пользователя по токену."""
 
-    reader, writer = await asyncio.open_connection(DEFAULT_SERVER_HOST, DEFAULT_SERVER_PORT)
-    data = await reader.readline()
-
-    # Получаем первое сообщение из чата
-    received_message = data.decode()
-    logging.debug(received_message)
-
-    # Отправляем хеш пользователя
-    sent_message = f'{CHAT_HASH}\n'
-    logging.debug(sent_message)
-    writer.write(sent_message.encode())
-    await writer.drain()
+    await submit_message(writer, token)
 
     # Получаем сообщение и проверяем верно ли авторизовались
-    data = await reader.readline()
-    received_message_json = json.loads(data.decode())
-    logging.debug(received_message_json)
+    received_message_json = await read_message_json(reader)
 
     if received_message_json is None:
-        print('Неизвестный токен. Проверьте его или зарегистрируйте заново.')
-        # Получаем строку о вводе логина нового пользователя
-        data = await reader.readline()
-        logging.debug(data.decode())
+        print('Неизвестный токен.')
+        return False
+    return True
 
-        # Регистрируем нового пользователя
-        sent_message = 'User\n'
-        logging.debug(sent_message)
-        writer.write(sent_message.encode())
-        await writer.drain()
 
-        # Получаем сообщение и сохранем хеш
-        data = await reader.readline()
-        received_message_json = json.loads(data.decode())
-        logging.debug(received_message_json)
+async def register(writer, reader):
+    """Регистрируем нового пользователя и возаращем токен"""
+    # Получаем строку о вводе логина нового пользователя
+    data = await reader.readline()
+    logging.debug(data.decode())
 
-        new_hash = received_message_json.get('account_hash')
-        print(f'Ваш новый токен: {new_hash}')
-        return
+    # Регистрируем нового пользователя
+    await submit_message(writer, 'User')
 
-    # Отправляем сообщение в чат
-    sent_message = 'Hello\n\n'
+    # Получаем сообщение и сохранем хеш
+    received_message_json = await read_message_json(reader)
+
+    token = received_message_json.get('account_hash')
+    print(f'Ваш новый токен: {token}')
+
+    # Вычитываем строку о вводе нового сообщения
+    await read_message_str(reader)
+
+
+async def submit_message(writer, message):
+    """Отправялем сообщение в чат"""
+    sent_message = f'{message}\n'
     logging.debug(sent_message)
     writer.write(sent_message.encode())
     await writer.drain()
+
+
+async def read_message_str(reader):
+    """Читаем сообщение из чата и возвращаем строковое представление."""
+    data = await reader.readline()
+    received_message = data.decode()
+    logging.debug(received_message)
+    return data
+
+
+async def read_message_json(reader):
+    """Читаем сообщение и возвращаем json."""
+    received_message_str = await read_message_str(reader)
+    return json.loads(received_message_str)
+
+
+async def main():
+    reader, writer = await asyncio.open_connection(DEFAULT_SERVER_HOST, DEFAULT_SERVER_PORT)
+
+    # Получаем первое сообщение из чата
+    await read_message_str(reader)
+
+    is_authorise = await authorise(writer, reader, CHAT_HASH)
+
+    if is_authorise is False:
+        await register(writer, reader)
+
+    await submit_message(writer, 'Hello')
 
     writer.close()
     await writer.wait_closed()
