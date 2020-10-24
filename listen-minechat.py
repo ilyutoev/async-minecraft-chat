8ea81ee924f8de46f735b3950265be263f5d2981
ilyutoev <ilyutoev@gmail.com>
2020-10-24 23:51:16 +0300

    Обработка исключения ошибки подключения

--- listen-minechat.py
+++ listen-minechat.py
@@ -2,11 +2,14 @@ import asyncio
 from datetime import datetime
 import argparse
 import os
+import logging
+import time
 
 import aiofiles
 
 from connection_helper import open_connection
 
+logger = logging.getLogger(__name__)
 
 DEFAULT_SERVER_HOST = os.getenv('MINECHAT_SERVER_HOST', 'minechat.dvmn.org')
 DEFAULT_SERVER_PORT = os.getenv('MINECHAT_SERVER_PORT', 5000)
@@ -23,17 +26,29 @@ def get_arguments():
 
 
 async def main():
+    logger.setLevel(logging.DEBUG)
     args = get_arguments()
 
-    async with open_connection(args.host, args.port) as (reader, writer):
-        async with aiofiles.open(args.history, mode='a') as f:
-            while True:
-                data = await reader.readline()
-                if not data:
-                    break
+    connect_attempts = 0
+    while True:
+        try:
+            async with open_connection(args.host, args.port) as (reader, writer):
+                async with aiofiles.open(args.history, mode='a') as f:
+                    while True:
+                        data = await reader.readline()
+                        if not data:
+                            break
 
-                await f.write(f'[{datetime.now().strftime("%d.%m.%Y %H:%M")}] {data.decode()}')
-                print(data.decode())
+                        await f.write(f'[{datetime.now().strftime("%d.%m.%Y %H:%M")}] {data.decode()}')
+                        print(data.decode())
+        except KeyboardInterrupt:
+            raise
+        except Exception as e:
+            # Непонятно какой ловить эксепшен при орыве соединения, тк отключив сеть локально, скрипт продолжает работать
+            logger.exception(f'Read exception: {e}', exc_info=True)
+
+            time.sleep(connect_attempts)
+            connect_attempts += 1
 
 if __name__ == '__main__':
     asyncio.run(main())
